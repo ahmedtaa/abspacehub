1ac358e79a4965c910d70ba3c1ff235ef945339e
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-03-09 00:04:35 +0200

    Done with fetch and nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchData } from './redux/missions/missions';
+import Header from './components/Header';
 import RocketsList from './components/rockets/RocketsList';
 import Profile from './components/profile/Profile';
 import DragonList from './components/dragons/DragonList';
 import MissionsList from './components/missions/MissionsList';
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
   return (
     <Router>
       <Header />
@@ -17,7 +24,6 @@ export default function App() {
         <Route path="/dragons" element={<DragonList />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-
     </Router>
   );
 }
