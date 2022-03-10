562576a940e280332def9bc4dec8511f1436adef
unknown <butlermuwo@gmail.com>
2022-03-10 11:07:18 +0200

    change css modules

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDragons } from '../../redux/dragons/dragons';
-import styles from './styles/Profile.module.css';
 
 const Profile = () => {
   const dragons = useSelector((state) => state.dragons.dragons);
@@ -16,12 +15,12 @@ const Profile = () => {
   const reserveData = dragons.filter((data) => data.reserved === true && data);
   return (
     <>
-      <section className={styles.profileSection}>
-        <div className={styles.dragonsCard}>
-          <h2 className={styles.dragonsName}>My Rockets</h2>
+      <section className="profileSection">
+        <div className="dragonsCard">
+          <h2 className="dragonsName">My Dragons</h2>
           {reserveData.map((dragon) => (
             <div key={dragon.id}>
-              <h4 className={styles.name}>{dragon.name}</h4>
+              <h4 className="newName">{dragon.name}</h4>
             </div>
           ))}
         </div>
@@ -29,5 +28,21 @@ const Profile = () => {
     </>
   );
 };
+//   const reserveData = dragons.filter((data) => data.reserved === true && data);
+//   return (
+//     <>
+//       <section className={styles.profileSection}>
+//         <div className={styles.dragonsCard}>
+//           <h2 className={styles.dragonsName}>My Rockets</h2>
+//           {reserveData.map((dragon) => (
+//             <div key={dragon.id}>
+//               <h4 className={styles.name}>{dragon.name}</h4>
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
 
 export default Profile;
